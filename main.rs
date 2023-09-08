dc937bc9a8890e6e1d8ff18a1704c11f464bf978
Sonnur Gülen <96986979+Sonnurg@users.noreply.github.com>
2023-09-08 18:48:14 +0300

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,30 @@
+struct FilterCondition<T> {
+    value: T,
+}
+
+impl<T: PartialEq> FilterCondition<T> {
+    fn is_match(&self, item: &T) -> bool {
+        &self.value == item
+    }
+}
+
+fn custom_filter<T: Clone + PartialEq>(collection: &[T], condition: &FilterCondition<T>) -> Vec<T> {
+    collection
+        .iter()
+        .filter(|&item| condition.is_match(item))
+        .cloned()
+        .collect()
+}
+
+fn main() {
+    let my_string="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+    let characters: Vec<char> = my_string.chars().collect();
+    let my_collection: Vec<u32> =characters.iter().map(|&x| x as u32).collect();
+    let condition = FilterCondition { value: 'o' as u32 };
+    let filtered_vector = custom_filter(&my_collection, &condition); // Prefix with underscore
+// Define a function called custom_filter that takes a collection (e.g., a vector) and a reference to a FilterCondition object as arguments. The function should iterate over the elements in the collection and return a new collection containing only the elements that match the filter condition.
+
+    println!("Original Collection: {:?}", my_collection);
+    println!("Filtered Collection: {:?}", filtered_vector);
+    println!("Number of Filtered Collection: {:?}", filtered_vector.len());
+}
